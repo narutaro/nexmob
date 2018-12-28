942ed648339c7303cfce630282decfb6d13d0417
narutaro <narusan@mac.com>
2018-12-28 17:58:30 +0900

    rename

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-ons-navigator swipeable :page-stack="pageStack" @push-page="pageStack.push($event)">
+  </v-ons-navigator>
+</template>
+
+<script>
+  import login_page from './login.vue'
+  export default {
+    data() {
+      return {
+        pageStack: [login_page]
+      }
+    }
+  }
+</script>
